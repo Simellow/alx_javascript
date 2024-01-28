d5dd2b524d02c5038e8fc0f7e0835d6d817fcacd
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-01-28 20:37:57 +0300

    js

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -4,5 +4,12 @@ const myObject = {
     value: 12
   };
   console.log(myObject);
-  let newObj = str.replace("12", "89");
+  myObject.value = 89
   console.log(myObject);
+
+// let obj = {
+//     key1: "value1", 
+//     key2: "value2", 
+//     key3: "value3"};
+// obj.key2 = "new value";
+// console.log(obj);
