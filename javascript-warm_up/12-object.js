ee295f517f4fbaad58f956d081a64e577fdeb5e8
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-01-28 20:46:10 +0300

    js

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -7,9 +7,3 @@ const myObject = {
   myObject.value = 89
   console.log(myObject);
 
-// let obj = {
-//     key1: "value1", 
-//     key2: "value2", 
-//     key3: "value3"};
-// obj.key2 = "new value";
-// console.log(obj);
---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+//a function that returns the addition of 2 integers
+function add(num1, num2){
+    let result = num1 + num2;
+    return result;
+}
