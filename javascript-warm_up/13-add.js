197341411684370d71d8cc1bfa7cd4e80df4d492
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-01-29 12:04:27 +0300

    1

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 //a function that returns the addition of 2 integers
 
-function add(a, b) {
-    return a + b;
-  }
+let add = (a, b) => a + b
+
+//can also be written as
+// function add(a, b) {
+//     return a + b;
+//   }
