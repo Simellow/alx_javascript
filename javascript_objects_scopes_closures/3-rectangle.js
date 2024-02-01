cdc10f3420e1f7831f259907902c0de49063da33
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-02-01 15:23:41 +0300

    1

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -7,6 +7,7 @@ class Rectangle {
         this.height = h;
         }
     }
+    //instance method print that prints the rectangle using the character X
     print() {
         for (let i = 0; 
             i < this.height; 
@@ -18,13 +19,4 @@ class Rectangle {
 module.exports = Rectangle;
 
 
-
-
-// Write a class Rectangle that defines a rectangle:
-
-// You must use the class notation for defining your class
-// The constructor must take 2 arguments: w and h
-// Initialize the instance attribute width with the value of w
-// Initialize the instance attribute height with the value of h
-// If w or h is equal to 0 or not a positive integer, create an empty object
 // Create an instance method called print() that prints the rectangle using the character X
