ce209b90303936756369fce39d534a5f2661883a
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-02-01 09:59:37 +0300

    js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 //writing a class Rectangle that defines a rectangle
 class Rectangle {
     constructor (w, h){
-        if (w && h > 0){
+        if (w || h > 0){
         this.width = w;
         this.height = h;
         }
