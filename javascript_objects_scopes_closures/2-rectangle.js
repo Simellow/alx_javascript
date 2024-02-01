58925b98f8aff1c12e7cad7a958a884b4f51dc04
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-02-01 10:13:38 +0300

    1

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,13 +3,11 @@
 class Rectangle {
     constructor (w, h){
         if (w && h > 0){
-        }
         this.width = w;
         this.height = h;
+        }
     }
 }
-
-
 module.exports = Rectangle;
 
 
