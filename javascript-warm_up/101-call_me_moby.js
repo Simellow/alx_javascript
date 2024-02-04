54d495127850f4ee5343d33505492255219f5ee1
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-02-04 12:13:47 +0300

    1

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-exports.xTimes = function (x, aFunction) {
+exports.callMeMoby = function (x, theFunction) {
     for (let i = 0; i < x; i++) {
-        aFunction();
+      theFunction();
     }
-}
-
+  };
