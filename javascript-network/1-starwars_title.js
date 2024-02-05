420663f209feec5fd9dc79fb24f2fdc4f1968d0e
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-02-05 16:13:02 +0300

    1

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -7,7 +7,8 @@ const movieID = process.argv[2];
 //We create the complete API URL by appending the movie ID to the base URL (https://swapi-api.alx-tools.com/api/films/:id)
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
-request(apiUrl, (error, response, body) => {
+request(apiUrl, (error, response, body) => { //Inside this callback function, we have access to the response object, which contains information about the HTTP response
+    
     //Inside the callback, we check if there’s an error. If so, we log an error message
     if (error) {
         console.error('Error:', error.message);
