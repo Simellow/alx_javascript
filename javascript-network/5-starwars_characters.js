f76ef5db8f8b6d4c6b14f66021b3985617ea4a1f
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-02-06 21:27:59 +0300

    1

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
 
 const request = require('request');
-const starWarsUri = 'https://swapi-api.alx-tools.com/api/films/'.concat(process.argv[2]);
 
-request(starWarsUri, function (_err, _res, body) {
+const movieID = process.argv[2]; 
+
+const starWarsUri = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+request(starWarsUri, function (_error, request, body) {
   const characters = JSON.parse(body).characters;
 
   for (let i = 0; i < characters.length; ++i) {
-    request(characters[i], function (_cErr, _cRes, cBody) {
+    request(characters[i], function (_cError, _cRequest, _cBody) {
       console.log(JSON.parse(cBody).name);
     });
   }
