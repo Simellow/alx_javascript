2113135415461bb903a46f14a480096836da7377
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-02-06 17:12:17 +0300

    1

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -6,19 +6,15 @@ const fs = require('fs'); //importing the file system module
 const urlToFetch = process.argv[2]; 
 const filePath = process.argv[3]
 
-if (!urlToFetch) {
-    console.error('Usage: node fetch-and-save.js <URL>');
-    process.exit(1);
-  }
-  
-  // Make the request to fetch the content
-  request(urlToFetch, (error, response, body) => {
+
+// Make the request to fetch the content
+request(urlToFetch, (error, response, body) => {
     if (error) {
       console.error('Error fetching content:', error.message);
       return;
     }
   
     // Write the response body to the file
-    fs.writeFileSync(filePath, 'utf-8');
+    fs.writeFileSync(filePath, body, 'utf-8');
     console.log(`${filePath}`);
-  });
+});
