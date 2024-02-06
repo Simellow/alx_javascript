bb422d5633b4a6e9ccbc9f6ecadcf9032be3aa1b
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-02-06 18:48:41 +0300

    1

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,22 +1,21 @@
 #!/usr/bin/node
-const request = require('request'); 
+const request = require('request');
 
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
+const url = process.argv[2];
 
 const id = 18;
 
-request.get(apiUrl, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error.message);
-    } else {
-            const filmsData = JSON.parse(body);
-            for (let i = 0; filmsData.results[i] !== undefined; i++) {
-                if (filmsData.results[i].characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)) {
-                  count++;
-                }
-              }
-              console.log(count)
-            }
-     });
+request.get(url, function (err, response, body) {
+  let count = 0;
+  if (err) {
+    console.log(err);
+  }
+  const data = JSON.parse(body);
+  for (let i = 0; data.results[i] !== undefined; i++) {
+    if (data.results[i].characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)) {
+      count++;
+    }
+  }
+  console.log(count);
+});
 
- 
