5508e50e45cd12d11c596d50fec4d8ef81751e11
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-02-06 18:43:59 +0300

    1

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -5,9 +5,11 @@ const movieID = process.argv[2];
 
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
-request.get(apiUrl, (error, response, body) => {
+request(apiUrl, (error, response, body) => {
+
     if (error) {
         console.error('Error:', error.message);
+
     } else {
             const movieInfo = JSON.parse(body);
             console.log(`${movieInfo.title}`);
